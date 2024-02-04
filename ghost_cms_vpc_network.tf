132c95343d3abf2fb9fcee1e9d0bcc99491b3f79
Rocco Langeweg <rocco.langeweg@gmail.com>
2024-02-04 22:01:05 +0100

    updated net mask to 28

--- ghost_cms_vpc_network.tf
+++ ghost_cms_vpc_network.tf
@@ -7,7 +7,7 @@ resource "google_compute_network" "ghost_cms_vpc_network" {
 resource "google_compute_subnetwork" "ghost_cms_vpc_subnetwork" {
   name          = "ghost-cms-vpc-subnetwork"
   network       = google_compute_network.ghost_cms_vpc_network.id
-  ip_cidr_range = "10.2.0.0/16"
+  ip_cidr_range = "10.2.0.0/28"
 }
 
 resource "google_vpc_access_connector" "ghost_cms_vpc_connector" {
