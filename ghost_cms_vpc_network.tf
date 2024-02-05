257d38ee6130da1ef42a10cb55b2f62e2d472d16
Rocco Langeweg <rocco.langeweg@gmail.com>
2024-02-05 09:11:51 +0100

    removed google-beta provider from network resources

--- ghost_cms_vpc_network.tf
+++ ghost_cms_vpc_network.tf
@@ -4,7 +4,6 @@ resource "google_compute_network" "ghost_cms_vpc_network" {
 }
 
 resource "google_compute_global_address" "ghost_cms_vpc_global_address" {
-  provider      = google-beta
   name          = "ghost-cms-vpc-global-address"
   purpose       = "VPC_PEERING"
   address_type  = "INTERNAL"
@@ -13,7 +12,6 @@ resource "google_compute_global_address" "ghost_cms_vpc_global_address" {
 }
 
 resource "google_service_networking_connection" "private_vpc_connection" {
-  provider                = google-beta
   network                 = google_compute_network.ghost_cms_vpc_network.self_link
   service                 = "servicenetworking.googleapis.com"
   reserved_peering_ranges = [google_compute_global_address.ghost_cms_vpc_global_address.name]
